b9bbc04fbbf7362fde5f86147fd2b5d0b2797d11
Rc-2022 <Rc-2022@DESKTOP-6FJ0IM4>
2023-03-07 12:27:50 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp1;
+
+
+// Arrays
+/*int[] x = { 5, 6, 7, 8, 9 };
+string[] y = new string[10];
+
+var z = new int[10];
+
+float[] w;
+
+Console.WriteLine(x[0]);*/
+/*Console.WriteLine(x[1]);
+Console.WriteLine(x[2]);
+Console.WriteLine(x[3]);
+Console.WriteLine(x[x.Length - 1]);
+Console.WriteLine(x[^1]); // جيبلي قيمة من الآخر حسب الرقم
+Console.WriteLine(x[^2]); // جيبلي قيمة من الآخر حسب الرقم
+
+for (int i = 2; i <= 50; i = i + 2)
+{
+    Console.WriteLine(i);
+}*/
+
+ /*public class User {
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+}
+*/
+
+public record Post (string Id, string Title, string author, string content);
+/********************************************************/
+
+
+
+
---
+++ ConsoleApp1/recprds.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleApp1;
+/*var p = new Post("dfwjwojfoi21",
+    "post 1",
+    "post 1 content",
+    "Abed");
+Console.WriteLine(p);
+/*
+var colors = new string[] { "red", "green", "yello", "black" };
+for (var i = 0; i < colors.Length; i++)
+{
+    Console.WriteLine(colors[i]);
+}
+*/
+
+List<string> colors = new List<string>();
+Console.WriteLine(colors.Count);
+colors.Add("red");
+colors.Add("green");
+colors.Add("yello");
+colors.Add("black");
+colors.Add("white");
+colors[3] = "brown";
+Console.WriteLine(colors.Count);
+
+for (var i = 0; i < colors.Count; i++)
+{
+    Console.WriteLine(colors[i]);
+
+}
+Console.WriteLine("____________________");
+
+colors.RemoveAt(3);
+for (var i = 0; i < colors.Count; i++)
+{
+    Console.WriteLine(colors[i]);
+}
+
+
+
+
